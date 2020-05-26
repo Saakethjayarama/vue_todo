2c2d82f7ca5f7ecfdc1de97192bad849433a8373
saaketh <saakethaj@gmail.com>
2020-05-26 18:30:55 +0530

    files upload

---
+++ src/components/Techie.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="techie">
+        {{msg}}
+    </div>
+</template>
+<script>
+export default {
+    name: "Saaketh",
+    props: {
+        msg: String
+    }
+}
+</script>
+<style scoped>
+.techie{
+    color: tomato;
+}
+</style>
